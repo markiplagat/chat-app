9d565d5e6c3d49b233385c7e0501c203396638ab
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-01-30 23:31:53 +0300

    Rubocop correction

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
-    def index
-      @message = Message.new
-      @messages = Message.all
-    end
+  def index
+    @message = Message.new
+    @messages = Message.all
+  end
 end
